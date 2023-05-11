59d719c96535ed1d01ad6996420612dba0e50833
Anup <130830567+Anup9401@users.noreply.github.com>
2023-05-11 21:43:49 +0530

    assignment5

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,11 +3,11 @@
 case "$1" in
 	start)
 		echo "Starting AESD Server"
-		start-stop-daemon aesdsocket -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+		start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
 		;;
 	stop)
 		echo "Stopping AESD Server"
-		start-stop-daemon aesdsocket -K -n aesdsocket
+		start-stop-daemon -K -n aesdsocket
 		;;
 	*)
 		echo "USAGE:: $0 {start|stop}"
